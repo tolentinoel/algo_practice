2556e906778c280daf15af89c55680dd1db8ecf7
tolentinoel <rckrleyn@gmail.com>
2021-04-14 18:30:03 -0700

    isPalindrome a little tough

--- test.js
+++ test.js
@@ -315,3 +315,24 @@
 
 
 //   console.log(firstUniqChar("cc"))
+
+
+// _________________________________________________________________
+
+function isPalindrome(s){
+    s = s.toLowerCase();
+    s = s.replace(/[^a-zA-Z0-9]/g,''); //says negate characters which are other than a-z, A-z or 0-9
+    let i = 0
+    let j = s.length-1
+
+    while(i < j){
+        if (s[i++]!== s[j--]){
+
+            return false
+        }
+    }
+    return true;
+}
+
+let str = "race a car"
+console.log(isPalindrome(str))
