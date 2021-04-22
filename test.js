e204df3023202b43182baa39624d4f4bad493bac
tolentinoel <rckrleyn@gmail.com>
2021-04-22 14:44:09 -0700

    solving longestCommonPrefix

--- test.js
+++ test.js
@@ -339,35 +339,48 @@
 
 // _________________________________________________________________
 
-function isAnagram(s,t){
+// function isAnagram(s,t){
 
-    if (s.length !== t.length){
-        return false
+//     if (s.length !== t.length){
+//         return false
+//     }
+
+//     let lib = {}
+
+//     for (let i=0; i < s.length; i++){
+//         if(lib[s[i]]){
+//             lib[s[i]] += 1
+//         } else {
+//             lib[s[i]] = 1
+//         }
+//     }
+// this is to check if the key value pair exist on 2nd string using library object.
+//     for (let i = 0; i < t.length; i++) {
+//         if (lib[t[i]]) {
+//           lib[t[i]] -= 1;
+//         } else {
+//           return false;
+//         }
+//     }
+
+//     return true;
+
+
+// }
+
+// let str = "anagram"
+// let test = "nagaram"
+// console.log(isAnagram(str, test))
+
+
+function longestCommonPrefix(arr){
+    let result = []
+    let j = 0
+    for(let i= 0; i < arr.length; i++){
+
     }
-
-    let lib = {}
-
-    for (let i=0; i < s.length; i++){
-        if(lib[s[i]]){
-            lib[s[i]] += 1
-        } else {
-            lib[s[i]] = 1
-        }
-    }
-
-    for (let i = 0; i < t.length; i++) {
-        if (lib[t[i]]) {
-          lib[t[i]] -= 1;
-        } else {
-          return false;
-        }
-    }
-
-    return true;
-
 
 }
 
-let str = "anagram"
-let test = "nagaram"
-console.log(isAnagram(str, test))
+let strings = ["flower","flow","flight"]
+console.log(longestCommonPrefix(strings))
