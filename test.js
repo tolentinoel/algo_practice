e12d2308c50e65cb259d76906f0c39de0087d741
tolentinoel <rckrleyn@gmail.com>
2021-04-22 17:25:56 -0700

    dictionary didnt work as properly with solution found

--- test.js
+++ test.js
@@ -373,31 +373,53 @@
 // console.log(isAnagram(str, test))
 
 
-function longestCommonPrefix(arr){
+function longestCommonPrefix(strs){
+    let dfault = '';
+    if (!strs.length) return dfault; //checks if arr is empty.
+    let word = strs[0]; //if array is not empty, declare variable for first word.
 
-    let obj = {}
+    for (let i=0; i< word.length; i++) {
 
-    for(let i= 0; i < arr.length; i++){
-      let a = arr[i].split("")
-
-        for(let j= 0; j <= 1; j++){
-            if(obj[a[j]]){
-                obj[a[j]] += 1
-            } else {
-                obj[a[j]] = 1
-            }
-        }
+        let x = strs.map((eachWord) => {
+            return eachWord.length > i && eachWord !== "" && eachWord[i] === word[i];
+        }); // creating a new array with conditionals:
+        // if eachWord's length is more than 1
+        // & its not an empty string
+        // & its first letter is equal to the first word's first letter.
+        
+        if (x.includes(false)) { return dfault; }
+        dfault += word[i];
     }
- 
-    let rr = Object.values(obj)
-    let final = ""
+    console.log(x, word[i])
+    return dfault;
 
-    if(rr[0] == 1){
-        return final
-    } else {
 
-        return Object.keys(obj).filter(key => obj[key] === rr[0] && obj[key] > 1).join("")
-    }
+    // if (!arr.length || arr.length === 1){
+    //     return '';
+    // }
+
+    // let obj = {}
+
+    // for(let i= 0; i < arr.length; i++){
+    //   let a = arr[i].split("")
+
+    //   for(let j= 0; j <= 1; j++){
+    //       if(obj[a[j]]){
+    //           obj[a[j]] += 1
+    //       } else {
+    //           obj[a[j]] = 1
+    //       }
+    //   }
+    // }
+
+    // let rr = Object.values(obj)
+
+    // if(rr[0] === 1){
+    //     return rr;
+    // }
+
+    //     return Object.keys(obj).filter(key => obj[key] === rr[0] && obj[key] > 1).join("")
+
 }
 
 // let strings = ["flower","flow","flight"]
