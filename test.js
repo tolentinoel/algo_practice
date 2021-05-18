d5979a8f68bf359c4c9ae7f68fa3cb4ccd717633
tolentinoel <rckrleyn@gmail.com>
2021-05-18 09:31:55 -0700

    commented out old leetcode

--- test.js
+++ test.js
@@ -428,9 +428,9 @@
 
 // _________________________________________________________________
 
-function removeElement(nums,val){
+// function removeElement(nums,val){
 
-
+    // SOLUTION 1 76ms 38MB
     // for (let i = 0; i < nums.length; i++){
     //     if(nums[i] === val){
     //         nums.splice(i, 1)
@@ -439,17 +439,16 @@ function removeElement(nums,val){
     //     }
     // return nums.length
 
-    
-    for (let i = nums.length; i >= 0; i--) {
-        if (nums[i] === val) {
-          nums.splice(i, 1);
-        }
-      } 
-// console.log(nums)
+    // SOLUTION 2 80ms 38MB
+//     for (let i = nums.length; i >= 0; i--) {
+//         if (nums[i] === val) {
+//           nums.splice(i, 1);
+//         }
+//       }
 
-}
 
-let n = [0,1,2,2,3,0,4,2]
+// }
 
-let v = 2
-console.log(removeElement(n,v))
+// let n = [0,1,2,2,3,0,4,2]
+// let v = 2
+// console.log(removeElement(n,v))
