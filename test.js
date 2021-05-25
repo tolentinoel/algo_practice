34a4602e39f39cf7aaa03aa88e8632d9896e8991
tolentinoel <rckrleyn@gmail.com>
2021-05-25 15:06:59 -0700

    solving other testcase 39/59

--- test.js
+++ test.js
@@ -457,6 +457,11 @@
 
 function merge(nums1, m, nums2, n){
 
+    nums1.splice(m, n)
+    nums2.map(num => {
+       nums1.push(num)
+    })
+    return nums1.sort((b,a)=> b-a)
 }
 
 let nums1 = [1,2,3,0,0,0]
