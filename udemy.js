957568172e3d5a655161aa0d7b5b1d45eaafa89f
tolentinoel <rckrleyn@gmail.com>
2021-05-26 22:29:30 -0700

    udemy example added

--- udemy.js
+++ udemy.js
@@ -1,7 +1,12 @@
 function addNum(n){
     let result = 0 
     for (let i = 0; i <= n ; i++){
-        total += i
+        result += i
     }
-    return total
+    return result
 }
+
+var t1 = performance.now()
+addNum(6)
+var t2 = performance.now()
+console.log((t2-t1)/1000)
