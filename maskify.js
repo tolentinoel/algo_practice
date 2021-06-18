b3364c7cb11f2f5ed6476499e5185598895c6ac2
tolentinoel <rckrleyn@gmail.com>
2021-06-17 23:26:04 -0700

    added reduceArray and maskify

---
+++ maskify.js
@@ -0,0 +1,22 @@
+function maskify(cc) {
+   let arr = cc.split("")
+   let result = []
+   if (arr.length <= 1){
+       return String(arr)
+   }else {
+        for(let i = 0; i < arr.length-4; i++){
+           arr[i]= "#"
+       }
+           console.log(arr.join(""))
+
+   }
+}
+
+maskify("4556364607935616")
+== "############5616"
+maskify("64607935616")
+==      "#######5616"
+maskify("1")
+==                "1"
+maskify("")
+==                 ""
