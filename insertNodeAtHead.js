7efb1a5d9f10993135221b7633363c3cbd37efd7
tolentinoel <rckrleyn@gmail.com>
2021-08-09 19:40:31 -0700

    Try insertNode challenge

---
+++ insertNodeAtHead.js
@@ -0,0 +1,18 @@
+function insertNodeAtHead(head, data) {
+    // built in function SinglyLinkedListNode passing in data
+    // We know that the head is next to NULL
+
+    let newNode = new SinglyLinkedListNode(data);
+    // If no head, our initilized newNode is the head and we will return.
+    if (!head){
+        head = newNode
+        return head
+    // Else, we will insert a newNode next to head. Then we will return.
+    } else {
+        newNode.next = head
+        head = newNode
+    }
+    return head
+}
+// TEST CASES:
+// https://www.hackerrank.com/challenges/insert-a-node-at-the-head-of-a-linked-list/problem
