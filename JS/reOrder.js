d0b39fb260939e1be5f2506770c443e9ae3a9d23
tolentinoel <rckrleyn@gmail.com>
2021-10-22 15:45:04 -0700

    reOrder array challenge done

---
+++ JS/reOrder.js
@@ -0,0 +1,29 @@
+function reOrderArr (arr, idx) {
+// Given an array of objects A, and an array of indexes B, reorder the objects in array A with the given indexes in array B.
+
+    let lib = {}
+
+    for (let i = 0; i < arr.length; i++){
+
+            lib[arr[i]] = idx[i]
+
+    }
+
+    let cloneArr = [...arr]
+
+    for (let j = 0; j < cloneArr.length; j++){
+
+        let index = lib[cloneArr[j]] //3
+        let letter = cloneArr[j] //C
+        arr.splice(index, 1, letter)
+    }
+
+    return arr
+
+}
+
+
+let a = ["C", "D", "E", "F", "G", "H"];
+let b = [3, 0, 4, 1, 2, 5];
+console.log(reOrderArr(a, b))
+// a is now [D, F, G, C, E, H]
