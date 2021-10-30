272555998c26662e9478548d1a72fec01d304de5
tolentinoel <rckrleyn@gmail.com>
2021-10-29 17:32:34 -0700

    Add Simple arr algo

---
+++ JS/simpleArr.js
@@ -0,0 +1,13 @@
+function simpleArr(ar) {
+    let num = 0
+
+    for(let i = 0; i < ar.length; i++){
+        num += ar[i]
+    }
+
+    return num
+}
+
+// Given an array of integers, find the sum of its elements.
+
+// For example, if the array , , so return .
