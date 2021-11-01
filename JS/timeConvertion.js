747b16e159c83696ff7f41880cac16c0e6f5ad5e
tolentinoel <rckrleyn@gmail.com>
2021-11-01 14:28:57 -0700

    Update with instructions

--- JS/timeConvertion.js
+++ JS/timeConvertion.js
@@ -8,3 +8,13 @@ function timeConvert(s){
 }
 
 console.log(timeConvert('07:05:45PM'))
+
+// Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+
+// Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+// - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+
+// Example
+// Return '12:01:00PM'.
+
+// Return '00:01:00'.
