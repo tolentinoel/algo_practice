a23c461e2a4f628b2f4faa10be56456580fd69d0
tolentinoel <rckrleyn@gmail.com>
2022-01-10 15:19:18 -0800

    Added validations if s is truthy or 1 element

--- JS/longestSubStr.js
+++ JS/longestSubStr.js
@@ -1,22 +1,51 @@
 function longestSubStr(s) {
-    let str = []
-    let count = str.length
-    for (let i = 0; i < s.length; i++){
-        // if current element is not equal to the one next to it & if it's not included in str array.
-        // use count as score of the longest substring without repeating characters
-        if (s[i] !== s[i+1] && !str.includes(s[i])){
-            str.push(s[i])
-            count = str.length
-            console.log(count, "IN LOOP")
-        } else {
-            str.length = 0
-            str.push(s[i])
-        }
 
+    if (!s) {
+        return 0
     }
-    console.log(str)
-    console.log(count, "AFTER LOOP")
+    if (s.length === 1) {
+        return 1
+    }
+
+    let checked = []
+    let count = 0
+
+
+    for (let i = 0; i < s.length; i++){
+
+        // if current element is not same as the next & if it's not included in checked array,
+        // push into checked array to keep track & add score using count
+        // if it exists in checked array, empty out array and start new sequence of elements using checked array.
+        // console.log(s[i])
+
+        if (s[i] !== s[i+1] && !checked.includes(s[i])){
+            checked.push(s[i])
+            count = checked.length
+
+        } else if (s[i] === s[i+1] && !checked.includes(s[i])){
+            checked.push(s[i])
+            count = checked.length
+
+        } else if (s[i] !== s[i+1] && checked.includes(s[i])){
+            checked.length = 0
+            checked.push(s[i])
+
+        } else {
+
+            checked.length = 0
+            checked.push(s[i])
+        }
+    }
+
+    console.log(checked)
+    // if count is higher than the length of the checked array, we found our length!
+    // else, we'll return the length of the checked array
+    return count >= checked.length ? count : checked.length
 }
-console.log(longestSubStr("abcabcbb"))
-console.log(longestSubStr("pwwkew"))
-console.log(longestSubStr("bbbbb"))
+
+// console.log(longestSubStr("abcabcbb"))
+console.log(longestSubStr("dvdf"))
+// console.log(longestSubStr("cdd"))
+
+
+
