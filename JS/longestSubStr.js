ca0c93e88f1a957cf4dba2ea63ad564e4748684b
tolentinoel <rckrleyn@gmail.com>
2022-01-10 11:44:50 -0800

    refining longestSubstr solution

--- JS/longestSubStr.js
+++ JS/longestSubStr.js
@@ -1,16 +1,22 @@
 function longestSubStr(s) {
     let str = []
-    let count = 0
+    let count = str.length
     for (let i = 0; i < s.length; i++){
-        if (s[i] !== s[i+1] && !str.includes(s[i]) ){
+        // if current element is not equal to the one next to it & if it's not included in str array.
+        // use count as score of the longest substring without repeating characters
+        if (s[i] !== s[i+1] && !str.includes(s[i])){
             str.push(s[i])
-            count += 1
+            count = str.length
+            console.log(count, "IN LOOP")
         } else {
             str.length = 0
             str.push(s[i])
         }
-        // console.log(str)
+
     }
-    return count
+    console.log(str)
+    console.log(count, "AFTER LOOP")
 }
 console.log(longestSubStr("abcabcbb"))
+console.log(longestSubStr("pwwkew"))
+console.log(longestSubStr("bbbbb"))
