9e1d470e5b85c1ed63278af8bf380c3d0462edc8
tolentinoel <rckrleyn@gmail.com>
2022-01-10 23:05:29 -0800

    Added runtime in comments

--- JS/consecutiveOne.js
+++ JS/consecutiveOne.js
@@ -1,6 +1,7 @@
 function consecutiveOne(nums){
     let list = []
     let score = 0
+    console.time("Runtime");
 
     for (let i = 0; i < nums.length; i++){
 
@@ -14,6 +15,12 @@ function consecutiveOne(nums){
     }
     list.push(score)
     // Once the loop ends, we have our collection of scores, next thing is to find the largest score from list.
+    
+    console.timeEnd("Runtime");
     return list.sort((a,b)=> b-a)[0]
 }
 console.log(consecutiveOne([1,1,0,1,1,1]))
+
+// Runtime: 92 ms
+// 42/42 testcases
+// Memory Usage: 43.1 MB
